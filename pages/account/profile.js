5fe65e5ad2fb8b1a989df57babdd285bd351db64
ilyamed <0ilya@bk.ru>
2022-06-10 21:53:16 +0300

    add loader for images

--- pages/account/profile.js
+++ pages/account/profile.js
@@ -22,6 +22,10 @@ const CardTitle = () => {
  );
 };
 
+const myLoader = ({ src, width, quality }) => {
+ return `https://images.unsplash.com/${src}`;
+};
+
 const Profile = () => (
  <div className="profile-wrapper">
   <Card
@@ -29,11 +33,13 @@ const Profile = () => (
    className="userCard"
    cover={
     <Image
+     loader={myLoader}
      layout="responsive"
-     sizes="60vw"
+     width="50px"
+     height="50px"
      className="avatarBig"
-     alt="example"
-     src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+     alt="Ваше фото"
+     src="photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
     />
    }
    actions={[
