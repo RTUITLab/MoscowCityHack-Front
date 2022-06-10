ddaf0a576e3182330b2cc8b2fcd8cd92a2a3889a
Kirill <kirill.baikin@gmail.com>
2022-06-10 20:57:40 +0300

    Layout changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import '../styles/globals.css';
 import 'antd/dist/antd.css';
+import Layout from "../components/Layout/Layout";
 
 function MyApp({ Component, pageProps }) {
- return <Component {...pageProps} />;
+ if(Component.prototype.type==="Loginpage")
+  return <Component {...pageProps} />;
+ else
+  return <Layout><Component {...pageProps}/></Layout>
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -56,3 +56,5 @@ export default function Home() {
 		</div>
 	);
 }
+
+Home.prototype.type="LoginPage"
