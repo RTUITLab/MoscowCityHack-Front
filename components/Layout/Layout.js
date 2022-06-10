c792fb0ca5acad74627ff4ded7358fd0fc378952
Kirill <kirill.baikin@gmail.com>
2022-06-10 21:29:23 +0300

    Layout changes

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,8 +1,28 @@
+import Header from "../Header/Header";
+import {Menu} from "antd";
+import styles from './Layout.module.scss'
+
 export default function Layout({children}){
 	return(
-		<div>
-			fdsffffffff
-			{children}
+		<div className={styles.parent}>
+			<div>
+				<Header/>
+			</div>
+			<div className={styles.content}>
+				<div>
+					<Menu>
+						<Menu.Item>item 1</Menu.Item>
+						<Menu.Item>item 2</Menu.Item>
+						<Menu.SubMenu title="sub menu">
+							<Menu.Item>item 3</Menu.Item>
+						</Menu.SubMenu>
+					</Menu>
+				</div>
+				<div>
+					<h2>Заголовок</h2>
+					{children}
+				</div>
+			</div>
 		</div>
 	)
 }
