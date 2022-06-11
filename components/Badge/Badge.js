b6b878f52dd10448a4a86bfeec64d586299aa0dd
ilyamed <0ilya@bk.ru>
2022-06-11 18:52:03 +0300

    objectFit fix

--- components/Badge/Badge.js
+++ components/Badge/Badge.js
@@ -1,16 +1,11 @@
 import styles from './Badge.module.scss';
-import Image from 'next/image';
 
 export default function Badge(props) {
  return (
   <div>
    <div className={styles.badgeWrapper}>
-    <img
-     alt="badge"
-     src={"/images/badge.png"}
-     width="28px"
-     objectFit="cover"></img>
-    <span style={{marginLeft:"10px"}}>{props.count}</span>
+    <img alt="badge" src={'/images/badge.png'} width="28px"></img>
+    <span style={{ marginLeft: '10px' }}>{props.count}</span>
    </div>
   </div>
  );
