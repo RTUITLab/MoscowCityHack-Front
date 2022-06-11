6091fdea47f9569a95a3f5ede4caa97b62e49d3f
ilyamed <0ilya@bk.ru>
2022-06-11 12:49:40 +0300

    add link to shop and configure title

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,59 +1,108 @@
-import Header from "../Header/Header";
-import {Menu} from "antd";
-import styles from './Layout.module.scss'
-import {MailOutlined, ShoppingCartOutlined, UserOutlined} from "@ant-design/icons";
-import {useContext, useEffect, useState} from "react";
-import {useRouter} from "next/router";
-import MainContext from "../../contexts/MainContext";
+import Header from '../Header/Header';
+import { Menu } from 'antd';
+import styles from './Layout.module.scss';
+import {
+ MailOutlined,
+ ShoppingCartOutlined,
+ UserOutlined,
+ ShoppingFilled,
+ HeartFilled,
+} from '@ant-design/icons';
+import { Button } from 'antd';
 
-export default function Layout({children}) {
-	const router = useRouter()
-	const [activeTab, setActiveTab] = useState("profile")
-	const [title, setTitle]=useState("Профиль")
+import { useContext, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import MainContext from '../../contexts/MainContext';
+import Badge from '../Badge/Badge';
 
-	useEffect(() => {
-		let currentHref = router.pathname
-		if (currentHref===("/account/profile")) {
-			setActiveTab("profile")
-			setTitle("Профиль")
-		} else if (currentHref===("/account/events")) {
-			setActiveTab("events")
-			setTitle("Мероприятия")
-		} else if (currentHref===("/account/events/create")) {
-			setActiveTab("events")
-			setTitle("Создание нового мероприятия")
-		}else {
-			//	 тут магаз
-		}
-	}, [router.pathname])
+export default function Layout({ children }) {
+ const router = useRouter();
+ const [activeTab, setActiveTab] = useState('profile');
+ const [title, setTitle] = useState('Профиль');
 
-	return (
-		<div className={styles.parent}>
-			<div>
-				<Header/>
-			</div>
-			<div className={styles.content}>
-				<div style={{flex:"0 0 250px"}}>
-					<Menu
-						style={{height:"100%"}}
-						selectedKeys={[activeTab]}
-					>
-						<Menu.Item onClick={() => {
-							router.push("/account/profile")
-						}} key={"profile"} icon={<UserOutlined/>}>Профиль</Menu.Item>
-						<Menu.Item onClick={() => {
-							router.push("/account/events")
-						}} key={"events"} icon={<MailOutlined/>}>Мероприятия</Menu.Item>
-						<Menu.Item key={"shop"} icon={<ShoppingCartOutlined/>}>Лавка волонтера</Menu.Item>
-					</Menu>
-				</div>
-				<div style={{padding:"30px", marginBottom:"50px"}}>
-					<h2>{title}</h2>
-					<div >
-						{children}
-					</div>
-				</div>
-			</div>
-		</div>
-	)
+ useEffect(() => {
+  let currentHref = router.pathname;
+  if (currentHref === '/account/profile') {
+   setActiveTab('profile');
+   setTitle('Профиль');
+  } else if (currentHref === '/account/events') {
+   setActiveTab('events');
+   setTitle('Мероприятия');
+  } else if (currentHref === '/account/events/create') {
+   setActiveTab('events');
+   setTitle('Создание нового мероприятия');
+  } else if (currentHref === '/account/shop') {
+   setActiveTab('shop');
+   setTitle(
+    <div
+     style={{
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+     }}>
+     Лавка волонтера
+     <div
+      style={{
+       display: 'flex',
+       justifyContent: 'space-around',
+       alignItems: 'center',
+      }}>
+      <Button type="text">
+       <HeartFilled />
+       Избранное
+      </Button>
+      <Button type="text">
+       <ShoppingFilled />
+       Корзина
+      </Button>
+      <Badge count={123} />
+     </div>
+    </div>
+   );
+  } else {
+   //	 тут ?
+  }
+ }, [router.pathname]);
+
+ return (
+  <div className={styles.parent}>
+   <div>
+    <Header />
+   </div>
+   <div className={styles.content}>
+    <div style={{ flex: '0 0 250px' }}>
+     <Menu style={{ height: '100%' }} selectedKeys={[activeTab]}>
+      <Menu.Item
+       onClick={() => {
+        router.push('/account/profile');
+       }}
+       key={'profile'}
+       icon={<UserOutlined />}>
+       Профиль
+      </Menu.Item>
+      <Menu.Item
+       onClick={() => {
+        router.push('/account/events');
+       }}
+       key={'events'}
+       icon={<MailOutlined />}>
+       Мероприятия
+      </Menu.Item>
+      <Menu.Item
+       onClick={() => {
+        router.push('/account/shop');
+       }}
+       key={'shop'}
+       icon={<ShoppingCartOutlined />}>
+       Лавка волонтера
+      </Menu.Item>
+     </Menu>
+    </div>
+    <div style={{ padding: '30px', marginBottom: '50px' }}>
+     <h2>{title}</h2>
+     <div>{children}</div>
+    </div>
+   </div>
+  </div>
+ );
 }
