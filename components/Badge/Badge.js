2822f8ab840a0445900329346fb201d8d7b308e2
ilyamed <0ilya@bk.ru>
2022-06-11 12:48:48 +0300

    badge styling

--- components/Badge/Badge.js
+++ components/Badge/Badge.js
@@ -1,4 +1,19 @@
 import styles from './Badge.module.scss';
+import badge from '../../public/images/badge.png';
+import Image from 'next/image';
+
 export default function Badge(props) {
- return <div className={styles.badge}>{props.count}</div>;
+ return (
+  <div>
+   <div className={styles.badgeWrapper}>
+    <Image
+     alt="badge"
+     src={badge}
+     width="48"
+     height="55"
+     objectFit="cover"></Image>
+    <span>{props.count}</span>
+   </div>
+  </div>
+ );
 }
