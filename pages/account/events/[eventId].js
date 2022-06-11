d245d25fae43fc3422a40cb177e9c48304adf8d4
ilyamed <0ilya@bk.ru>
2022-06-11 22:54:57 +0300

    styling eventPage

--- pages/account/events/[eventId].js
+++ pages/account/events/[eventId].js
@@ -16,6 +16,7 @@ export default function EventPage() {
   try {
    setProject({
     title: 'Волонтеры охотники за приведениями',
+    online: 'online',
     imgSrc:
      'https://images.unsplash.com/photo-1634840647366-e755dc76882d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1620&q=80',
     directions: [
@@ -69,22 +70,40 @@ export default function EventPage() {
     <div className={styles.eventWrapper}>
      <header className={styles.projectHead}>
       <h2>{project.title}</h2>
+      <div className={styles.projectStatus}>
+       {project.online === 'online' ? (
+        <>
+         <img src={'/images/home.svg'} width={25} height={25} />
+         Онлайн
+        </>
+       ) : (
+        <>
+         <img src={'/images/parent.svg'} width={25} height={25} />
+         Оффлайн
+        </>
+       )}
+      </div>
       {
-       //</header>Button size="large">Участники</Button>
+       //Button size="large">Участники</Button>
       }
      </header>
      <section className={styles.projectTop}>
-      <Image src={project.imgSrc} width={700} />
+      <Image src={project.imgSrc} width={600} />
       <div className={styles.imgAside}>
        <h4>Направления</h4>
        <div className={styles.directions}>
         {project.directions.map((direction, i) => (
-         <Button key={i} size="large" className={styles.direction}>
+         <Button
+          key={i}
+          size="large"
+          className={styles.direction}
+          icon={
+           <img src="/images/plant.svg" style={{ backgroundColor: 'white' }} />
+          }>
           {direction}
          </Button>
         ))}
        </div>
-       <Divider />
        <div className={styles.texts}>
         <div className={styles.text}>
          <InfoCircleOutlined />
@@ -107,10 +126,8 @@ export default function EventPage() {
         status="active"
         percent={(project.capacity[0] / project.capacity[1]) * 100}
        />
-       <Divider />
-       <h4>Теги</h4>
-
        <div className={styles.tags}>
+        <h4>Теги: </h4>
         {project.tags.map((tag, i) => (
          <Tag key={i} className={styles.tag}>
           {tag}
@@ -145,9 +162,10 @@ export default function EventPage() {
         ))}
        </div>
       </ul>
+      <h3>Материалы</h3>
       <div className={styles.materials}>
        {project.materials.map((img, i) => (
-        <Image src={img} key={i} width={400} />
+        <Image src={img} key={i} width={350} />
        ))}
       </div>
      </section>
