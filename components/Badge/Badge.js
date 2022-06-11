97345587efbbdfe13a567266c8cab4a14b62c6ea
Kirill <kirill.baikin@gmail.com>
2022-06-11 13:51:37 +0300

    Profile updates

--- components/Badge/Badge.js
+++ components/Badge/Badge.js
@@ -1,18 +1,16 @@
 import styles from './Badge.module.scss';
-import badge from '../../public/images/badge.png';
 import Image from 'next/image';
 
 export default function Badge(props) {
  return (
   <div>
    <div className={styles.badgeWrapper}>
-    <Image
+    <img
      alt="badge"
-     src={badge}
-     width="48"
-     height="55"
-     objectFit="cover"></Image>
-    <span>{props.count}</span>
+     src={"/images/badge.png"}
+     width="28px"
+     objectFit="cover"></img>
+    <span style={{marginLeft:"10px"}}>{props.count}</span>
    </div>
   </div>
  );
--- pages/account/profile.js
+++ pages/account/profile.js
@@ -1,204 +1,185 @@
-import {
- EditOutlined,
- EllipsisOutlined,
- SettingOutlined,
-} from '@ant-design/icons';
+import {EditOutlined, EllipsisOutlined, SettingOutlined,} from '@ant-design/icons';
 import Image from 'next/image';
-import {
- Descriptions,
- Card,
- Progress,
- Statistic,
- Row,
- Col,
- Button,
- Table,
-} from 'antd';
+import {Button, Card, Col, Descriptions, Progress, Row, Statistic, Table,} from 'antd';
 import React from 'react';
-const { Meta } = Card;
 import Badge from '../../components/Badge/Badge';
 import styles from '../../styles/profile.module.scss';
 
+const {Meta} = Card;
+
 const CardTitle = () => {
- return (
-  <div className={styles.cardTitle}>
-   <h3 className={styles.name}>Костик Иванов</h3>
-   <div className={styles.status}>
-    <div className={styles.statusBarHeader}>200 до след. уровня</div>
-    <Progress percent={70} className={styles.statusBar} showInfo={false} />
-   </div>
-   <Badge count={2} />
-  </div>
- );
+	return (
+		<div className={styles.cardTitle}>
+			<h3 className={styles.name}>Костик Иванов</h3>
+			<div className={styles.status}>
+				<Progress percent={70} className={styles.statusBar} showInfo={false}/>
+				<div className={styles.statusBarHeader}>200 до след. уровня</div>
+				{/*<Badge count={2} />*/}
+			</div>
+		</div>
+	);
 };
 
-const myLoader = ({ src, width, quality }) => {
- return `https://images.unsplash.com/${src}`;
+const myLoader = ({src, width, quality}) => {
+	return `https://images.unsplash.com/${src}`;
 };
 
 const Profile = () => {
- const columns = [
-  {
-   title: 'Доброе дело',
-   dataIndex: 'type',
-   key: 'Доброе дело',
-   render: (text) => <span>{text}</span>,
-  },
-  {
-   title: 'Время работы',
-   dataIndex: 'time',
-   key: 'Время работы',
-   render: (text) => text + ' ч',
-  },
-  {
-   title: 'Дата начала',
-   dataIndex: 'timeStart',
-   key: 'Дата начала',
-  },
-  {
-   title: 'Дата конца',
-   dataIndex: 'timeEnd',
-   key: 'Дата конца',
-  },
-  {
-   title: 'Баллы',
-   dataIndex: 'points',
-   key: 'Баллы',
-   render: (points) => <Badge count={points} />,
-  },
- ];
+	const columns = [
+		{
+			title: 'Доброе дело',
+			dataIndex: 'type',
+			key: 'Доброе дело',
+			render: (text) => <span>{text}</span>,
+		},
+		{
+			title: 'Время работы',
+			dataIndex: 'time',
+			key: 'Время работы',
+			render: (text) => text + ' ч',
+		},
+		{
+			title: 'Дата начала',
+			dataIndex: 'timeStart',
+			key: 'Дата начала',
+		},
+		{
+			title: 'Дата конца',
+			dataIndex: 'timeEnd',
+			key: 'Дата конца',
+		},
+		{
+			title: 'Баллы',
+			dataIndex: 'points',
+			key: 'Баллы',
+			render: (points) => <Badge count={points}/>,
+		},
+	];
 
- const data = [
-  {
-   key: '1',
-   type: 'Помощь инвалидам',
-   time: 4,
-   timeStart: '13.01.2022',
-   timeEnd: '20.02.2022',
-   points: 100,
-  },
-  {
-   key: '2',
-   type: 'Помощь инвалидам',
-   time: 4,
-   timeStart: '13.01.2022',
-   timeEnd: '20.02.2022',
-   points: 200,
-  },
-  {
-   key: '3',
-   type: 'Помощь инвалидам',
-   time: 4,
-   timeStart: '13.01.2022',
-   timeEnd: '20.02.2022',
-   points: 350,
-  },
- ];
- return (
-  <div className={styles.profileWrapper}>
-   <Row justify="space-around">
-    <Col span={6}>
-     <Card
-      className={styles.userCard}
-      cover={
-       <>
-        <CardTitle />
-        <Image
-         loader={myLoader}
-         layout="responsive"
-         width="100"
-         height="100"
-         className={styles.avatarBig}
-         alt="Ваше фото"
-         src="photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
-        />
-       </>
-      }
-      actions={[
-       <SettingOutlined key="setting" />,
-       <EditOutlined key="edit" />,
-       <EllipsisOutlined key="ellipsis" />,
-      ]}>
-      <div className={styles.bottomStats}>
-       <Descriptions
-        column={1}
-        labelStyle={{
-         fontSize: '1.4em',
-        }}
-        contentStyle={{
-         display: 'flex',
-         justifyContent: 'flex-end',
-         fontSize: '1.4em',
-        }}>
-        <Descriptions.Item label="Количество мероприятий">
-         <span>{4}</span>
-        </Descriptions.Item>
-        <Descriptions.Item label="Часов работы">
-         <span>{12}</span>
-        </Descriptions.Item>
-        <Descriptions.Item label="Количество мероприятий">
-         <span>{4}</span>
-        </Descriptions.Item>
-       </Descriptions>
-      </div>
-     </Card>
-    </Col>
+	const data = [
+		{
+			key: '1',
+			type: 'Помощь инвалидам',
+			time: 4,
+			timeStart: '13.01.2022',
+			timeEnd: '20.02.2022',
+			points: 100,
+		},
+		{
+			key: '2',
+			type: 'Помощь инвалидам',
+			time: 4,
+			timeStart: '13.01.2022',
+			timeEnd: '20.02.2022',
+			points: 200,
+		},
+		{
+			key: '3',
+			type: 'Помощь инвалидам',
+			time: 4,
+			timeStart: '13.01.2022',
+			timeEnd: '20.02.2022',
+			points: 350,
+		},
+	];
+	return (
+		<div className={styles.profileWrapper}>
 
-    <Col span={14} className={styles.statsWrapper}>
-     <Row justify="start" align="top" wrap={false}>
-      <Col span={4} style={{ fontSize: 1.4 + 'em' }}>
-       <span>Баллы добра</span>
-       <Badge count={11283} />
-      </Col>
-      <Col span={4}>
-       <Statistic
-        title={<span style={{ fontSize: 1.4 + 'em' }}>Часов работы</span>}
-        value={25}
-        valueStyle={{ fontSize: 2 + 'em' }}
-       />
-      </Col>
-      <Col span={4}>
-       <Statistic
-        title={<span style={{ fontSize: 1.4 + 'em' }}>Мероприятия</span>}
-        value={5}
-        valueStyle={{ fontSize: 2 + 'em' }}
-       />
-      </Col>
-      <Col span={12} />
-     </Row>
-     <Row
-      justify="center"
-      align="middle"
-      style={{ marginTop: '5%' }}
-      className={styles.tableWrapper}>
-      <Row>
-       <Col span={14}>
-        <h3 className={styles.tableHeader}>Последние добрые дела</h3>
-       </Col>
-       <Col span={10}>
-        <Row justify="center" align="middle">
-         <Col span={12}>
-          <Button type="primary">Выгрузить в PDF</Button>
-         </Col>
-         <Col span={12}>
-          <Button type="primary">Достижения</Button>
-         </Col>
-        </Row>
-       </Col>
-      </Row>
-      <Row>
-       <Table
-        columns={columns}
-        dataSource={data}
-        pagination={{ position: ['bottomLeft'] }}
-       />
-      </Row>
-     </Row>
-    </Col>
-    <Col span={4} />
-   </Row>
-  </div>
- );
+			<Card
+				className={styles.userCard}
+				cover={
+					<>
+						<CardTitle/>
+						<Image
+							loader={myLoader}
+							layout="responsive"
+							width="100"
+							height="100"
+							className={styles.avatarBig}
+							alt="Ваше фото"
+							src="photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+						/>
+					</>
+				}
+				actions={[
+					<SettingOutlined key="setting"/>,
+					<EditOutlined key="edit"/>,
+					<EllipsisOutlined key="ellipsis"/>,
+				]}>
+				<div className={styles.bottomStats}>
+					<Descriptions
+						column={1}
+						labelStyle={{
+							fontSize: '1.4em',
+						}}
+						contentStyle={{
+							display: 'flex',
+							justifyContent: 'flex-end',
+							fontSize: '1.4em',
+						}}>
+						<Descriptions.Item label="Количество мероприятий">
+							<span>{4}</span>
+						</Descriptions.Item>
+						<Descriptions.Item label="Часов работы">
+							<span>{12}</span>
+						</Descriptions.Item>
+						<Descriptions.Item label="Количество мероприятий">
+							<span>{4}</span>
+						</Descriptions.Item>
+					</Descriptions>
+				</div>
+			</Card>
+
+			<div className={styles.statsWrapper}>
+				<div style={{display:"flex", justifyContent:"flex-start", gap:"30px"}}>
+					<div>
+						<span style={{color: "rgba(0, 0, 0, 0.45)", fontWeight:"400",fontSize:"20px"}}>Баллы добра</span>
+						<div style={{height:"2.5px"}}></div>
+						<div style={{display:"flex", marginLeft:"15px", alignItems:"center"}}>
+							<Badge/>
+							<span style={{fontSize:"2em"}}>26562</span>
+						</div>
+					</div>
+					<div>
+						<Statistic
+							title={<span style={{fontSize: 1.4 + 'em'}}>Часов работы</span>}
+							value={25}
+							valueStyle={{fontSize: 2 + 'em'}}
+						/>
+					</div>
+					<div>
+						<Statistic
+							title={<span style={{fontSize: 1.4 + 'em'}}>Мероприятия</span>}
+							value={5}
+							valueStyle={{fontSize: 2 + 'em'}}
+						/>
+					</div>
+				</div>
+				<Row
+					justify="center"
+					align="middle"
+					style={{marginTop: '5%'}}
+					className={styles.tableWrapper}>
+					<div style={{display: "flex", width: "100%", justifyContent: "space-between"}}>
+						<div style={{width: "100%"}}>
+							<h3 className={styles.tableHeader}>Последние добрые дела</h3>
+						</div>
+						<div style={{width: "100%", display: "flex", gap: "10px", justifyContent: "flex-end"}}>
+							<Button type="primary">Выгрузить в PDF</Button>
+							<Button type="primary">Достижения</Button>
+						</div>
+					</div>
+					<div style={{width:"100%"}}>
+						<Table
+							columns={columns}
+							dataSource={data}
+							pagination={{position: ['bottomLeft']}}
+						/>
+					</div>
+				</Row>
+			</div>
+		</div>
+	);
 };
 
 export default Profile;
