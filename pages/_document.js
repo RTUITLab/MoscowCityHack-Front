6549c7f354acc44f6dcb36c5fed60d6f1c37ad66
Kirill <kirill.baikin@gmail.com>
2022-06-12 12:03:06 +0300

    Fixes for production

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
-	return (
-		<Html>
-			<Head>
-				<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=a8693c98-e820-425a-948f-17952a243766" type="text/javascript"></script>
-			</Head>
-			<body>
-			<Main />
-			<NextScript />
-			</body>
-		</Html>
-	)
+ return (
+  <Html>
+   <Head></Head>
+   <body>
+    <Main />
+    <NextScript />
+   </body>
+  </Html>
+ );
 }
