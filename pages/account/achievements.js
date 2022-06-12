7166a70b916b94d745f89c9b59a05823ce304093
ilyamed <0ilya@bk.ru>
2022-06-12 03:46:08 +0300

    remove lines

--- pages/account/achievements.js
+++ pages/account/achievements.js
@@ -1,11 +1,10 @@
 import styles from '../../styles/achievements.module.scss';
 import MainContext from '../../contexts/MainContext.js';
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 
 export default function Achievements() {
  const [state, setState] = useContext(MainContext);
  const user = state.user;
- useEffect(() => console.log(state));
  return (
   <div className={styles.parentContainer}>
    {user.achievements.map((ach, i) => (
