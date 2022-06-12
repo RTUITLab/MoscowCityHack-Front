d37bddce7c9a54ba10f1a85bf52b9ea89e953619
ilyamed <0ilya@bk.ru>
2022-06-12 03:09:41 +0300

    add achievements

--- pages/account/achievements.js
+++ pages/account/achievements.js
@@ -1,5 +1,19 @@
 import styles from '../../styles/achievements.module.scss';
+import MainContext from '../../contexts/MainContext.js';
+import { useContext, useEffect } from 'react';
 
 export default function Achievements() {
- return <div className={styles.parentContainer}>f</div>;
+ const [state, setState] = useContext(MainContext);
+ const user = state.user;
+ useEffect(() => console.log(state));
+ return (
+  <div className={styles.parentContainer}>
+   {user.achievements.map((ach, i) => (
+    <div key={i} className={styles.achievement}>
+     <img src={ach.imgSrc} width={100} />
+     <div className={styles.description}>{ach.description}</div>
+    </div>
+   ))}
+  </div>
+ );
 }
