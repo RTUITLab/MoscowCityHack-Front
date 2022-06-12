40e0fb75214996e76f2e552f26e8f199e719b959
ilyamed <0ilya@bk.ru>
2022-06-12 10:14:43 +0300

    add profile in header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,19 +1,43 @@
 import styles from './Header.module.scss';
 import { useRouter } from 'next/router';
+import { useContext } from 'react';
+import MainContext from '../../contexts/MainContext';
+import { Popover, Button } from 'antd';
+import { ArrowDownOutlined } from '@ant-design/icons';
 
 export default function Header({ disableButtons }) {
+ const [state, useState] = useContext(MainContext);
  const router = useRouter();
 
  return (
   <div className={styles.parent}>
    {!disableButtons ? (
     <>
-     <span
+     <Button
+      type="text"
+      style={{ display: 'flex', color: 'white' }}
       onClick={() => {
        router.push('/');
       }}>
       Главная
-     </span>
+     </Button>
+     <Popover
+      title={state.user.name}
+      content={() => {
+       return (
+        <div style={{ display: 'grid', gap: '15px' }}>
+         <Button>Настройки</Button>
+         <Button>Выйти</Button>
+        </div>
+       );
+      }}
+      placement="bottom">
+      <Button type="text" className={styles.accountButton}>
+       <img src="/images/avatar.svg" width={30} />
+       <div>{state.user.name}</div>
+       <ArrowDownOutlined className={styles.arrow} />
+      </Button>
+     </Popover>
      {/*<span>Топ компаний</span>*/}
      {/*<span>Топ организаций</span>*/}
     </>
