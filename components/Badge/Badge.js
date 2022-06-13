76045bc247e0494c9b683cc5372318102a8fb14a
ilyamed <0ilya@bk.ru>
2022-06-13 06:22:51 +0300

    objectFit fix

--- components/Badge/Badge.js
+++ components/Badge/Badge.js
@@ -1,21 +1,20 @@
 import styles from './Badge.module.scss';
 
 export default function Badge(props) {
-
-  if(props.type==="level"){
-    return(
-      <div>
-        <div className={styles.badgeWrapper+" "+styles.level}>
-          <img
-            alt="badge"
-            src={"/images/blueBadge.svg"}
-            width="55px"
-            objectFit="cover"></img>
-          <span style={{position:"absolute"}}>{props.count}</span>
-        </div>
-      </div>
-    )
-  }
+ if (props.type === 'level') {
+  return (
+   <div>
+    <div className={styles.badgeWrapper + ' ' + styles.level}>
+     <img
+      alt="badge"
+      src={'/images/blueBadge.svg'}
+      width="55px"
+      style={{ objectFit: 'cover' }}></img>
+     <span style={{ position: 'absolute' }}>{props.count}</span>
+    </div>
+   </div>
+  );
+ }
  return (
   <div>
    <div className={styles.badgeWrapper}>
